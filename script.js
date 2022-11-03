687ca4f7f52ba12beec5fc4aacab05f5d3685e75
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-03 00:02:06 -0500

    Changed button class to id

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const startbtn = document.querySelector("button");
 startbtn.addEventListener("click", () => {
   window.location.href="./pages/page1-Quiz/index.html";
   console.log(button)
-})
+});
