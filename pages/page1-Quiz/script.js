64806193c7d8dcfdeac087941f36b62b3d9d4b26
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-02 23:15:31 -0500

    Removed css code from style.css

---
+++ pages/page1-Quiz/script.js
@@ -0,0 +1,10 @@
+// This page will need to display timer and each question one by one.
+// TODO: Diplay timer on screen to count down
+
+// TODO: Display Question #1 with the option to select an answer
+
+// TODO: After 'next' button is clicked, display question #2 with the option to select answer
+
+// TODO: After clicking 'next' button, display question #3 with the option to select answer
+
+// TODO: After clicking 'finish' button, display option to view score
