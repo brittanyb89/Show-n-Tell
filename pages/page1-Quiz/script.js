08f9af8d26f11fca0bac5187c548ae54c880e153
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-04 02:30:28 -0500

    Fixed event listener pathway

--- pages/page1-Quiz/script.js
+++ pages/page1-Quiz/script.js
@@ -18,6 +18,6 @@
 const startbtn = document.querySelector("button");
 
 startbtn.addEventListener("click", () => {
-  window.location.href="./pages/page1-Quiz/index.html";
+  window.location.href="../pages/page2-TestOver/index.html";
   console.log(button)
 });
