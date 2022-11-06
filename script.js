34fab0e9b89d9a93eacf0ec7aae793a513b2088c
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-06 12:24:11 -0600

    Added notes to script and html

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Start button will take user to next page with test questions
 const startbtn = document.querySelector("button");
 
 startbtn.addEventListener("click", () => {
   window.location.href="./pages/page1-Quiz/index.html";
   console.log(button)
 });
+
+// Once test is started, first question appears with choices, and timer starts
